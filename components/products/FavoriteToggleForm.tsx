7fbf6ed3bd9a5754114492c174465365ff6cdc6c
Ilya <stoynov.oleg@gmail.com>
2024-10-04 11:28:31 +0200

    commit

--- components/products/FavoriteToggleForm.tsx
+++ components/products/FavoriteToggleForm.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+'use client';
+import { usePathname } from 'next/navigation';
+import FormContainer from '../form/FormContainer';
+import { toggleFavoriteAction } from '@/utils/actions';
+import { CardSubmitButton } from '../form/Buttons';
 
-type Props = {}
+type FavoriteToggleFormProps = {
+  productId: string;
+  favoriteId: string | null;
+};
 
-const FavoriteToggleForm = () => {
+const FavoriteToggleForm = ({ 
+  productId,
+  favoriteId,
+}: FavoriteToggleFormProps) => {
+  const pathname = usePathname();
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    productId,
+    favoriteId,
+    pathname,
+  });
   return (
-    <div>
-        <h1>FavoriteToggleForm</h1>
-    </div>
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
   )
 }
 
