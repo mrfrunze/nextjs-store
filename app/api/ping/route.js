13883f6eddfb7f143d446eb6ef499559815dab78
Ilya <stoynov.oleg@gmail.com>
2025-01-06 19:18:18 +0100

    commit

--- app/api/ping/route.js
+++ app/api/ping/route.js
@@ -3,17 +3,18 @@ import prisma from '../../../utils/db';
 
 
 export async function HEAD() {
-  return new NextResponse("Pong", { status: 200 });
+  return new NextResponse(null, { status: 200 });
 }
 
 export async function GET(req) {
   try {
     // Простой запрос к базе данных
     await prisma.$queryRaw`SELECT 1`;
-    return new Response(JSON.stringify({ message: 'Database pinged successfully!' }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return new NextResponse("Pong", { status: 200 });
+    // return new Response(JSON.stringify({ message: 'Database pinged successfully!' }), {
+    //   status: 200,
+    //   headers: { 'Content-Type': 'application/json' },
+    // });
   } catch (error) {
     console.error('Error pinging database:', error);
     return new Response(JSON.stringify({ error: 'Failed to ping database' }), {
